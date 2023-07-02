4bb1c5b6e2d941082bba44cea4bfcb65303e9eb5
ruukhms <ruukhms@gmail.com>
2023-07-02 15:05:03 -0700

    first commit

---
+++ src/main/java/com/example/HOSTELBACKEND/HostelbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.HOSTELBACKEND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HOSTELBACKEND/HostelbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HOSTELBACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
